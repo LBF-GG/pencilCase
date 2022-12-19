d6d08f7573f1e36dff4e7b18d51134368d575e92
LBF-GG <354741708@qq.com>
2022-12-19 21:39:07 +0800

    Initial commit

---
+++ src/main/java/com/loaves/pencilcase/PencilCaseApplication.java
@@ -0,0 +1,13 @@
+package com.loaves.pencilcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PencilCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PencilCaseApplication.class, args);
+    }
+
+}
